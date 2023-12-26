09746c01e7d649a8de3ecd72a859627efa3e5f23
essalmimohamed1 <essalmimoha07@gmail.com>
2023-12-26 12:38:24 +0100

    name

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+// * Name
+let names = prompt("your name : ")
+function nameff(name) {
+    const name1 = name.split(" ");
+    for (let i = 0; i < name1.length; i++) {
+        name1[i] = name1[i][0].toUpperCase() + name1[i].substr(1).toLowerCase();
+    }
+    name1.join(" ");
+    let name2 = name1.toString().replace(/,/g, ' ');
+    console.log(name2);
+}
+if(names.length < 5){
+    let name = prompt("inter your name : ")
+    nameff(name)
+}
+else{
+    nameff(names)
+}
+// * email
+
