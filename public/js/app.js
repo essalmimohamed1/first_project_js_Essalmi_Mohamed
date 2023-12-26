e474067b42cd036e4a1497b4c8125253ebb25693
essalmimohamed1 <essalmimoha07@gmail.com>
2023-12-26 13:41:36 +0100

    email and age

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,33 @@
 // * Name
-let names = prompt("your name : ")
-function nameff(name) {
-    const name1 = name.split(" ");
-    for (let i = 0; i < name1.length; i++) {
-        name1[i] = name1[i][0].toUpperCase() + name1[i].substr(1).toLowerCase();
-    }
-    name1.join(" ");
-    let name2 = name1.toString().replace(/,/g, ' ');
-    console.log(name2);
-}
-if(names.length < 5){
-    let name = prompt("inter your name : ")
-    nameff(name)
-}
-else{
-    nameff(names)
-}
+// let names = prompt("your name : ")
+// function nameff(name) {
+//     const name1 = name.split(" ");
+//     for (let i = 0; i < name1.length; i++) {
+//         name1[i] = name1[i][0].toUpperCase() + name1[i].substr(1).toLowerCase();
+//     }
+//     name1.join(" ");
+//     let name2 = name1.toString().replace(/,/g, ' ');
+//     console.log(name2);
+// }
+// if(names.length < 5){
+//     let name = prompt("inter your name : ")
+//     nameff(name)
+// }
+// else{
+//     nameff(names)
+// }
 // * email
+// let email = prompt("your email : ") 
+// email = email.trim().toLowerCase()
+// if(email.length < 10){
+//     let email = prompt("your email : ") 
+//         console.log(email);
+//     }
+//     else{
+//         console.log(email);
+//     }
+// * age
+let age = prompt("your age : "); 
+age = Number(age.trim())
+alert(age)
 
